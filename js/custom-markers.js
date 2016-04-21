5f62aa37ff30fa8c436a87ec20382f329fce238f
TLDuhamel <timothee.duhamel@gmail.com>
2016-04-21 19:44:16 +1200

    First commit

---
+++ js/custom-markers.js
@@ -0,0 +1,74 @@
+var gateicon = L.icon({
+    type: 'gate',
+    iconUrl: './images/gate-icon.png',
+    iconSize:     [30, 30], // size of the icon
+    iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+
+
+GateMarker = L.Draw.Marker.extend({
+    options: {
+        icon: gateicon
+    }
+});
+
+function gate() {
+    var gateMarker = new GateMarker(map);
+    gateMarker.enable();
+}
+
+
+var troughIcon = L.icon({
+    type: 'trough',
+    iconUrl: './images/trough-icon.png',
+    iconSize:     [30, 30], // size of the icon
+    iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+
+
+TroughMarker = L.Draw.Marker.extend({
+    options: {
+        icon: troughIcon,
+        test: true
+    }
+});
+function trough() {
+    var gateMarker = new TroughMarker(map);
+    gateMarker.enable();
+}
+
+PaddockPoly = L.Draw.Polygon.extend({
+    options: {
+        allowIntersections: false
+    }
+});
+function field() {
+    var fieldPolygon = new PaddockPoly(map);
+    fieldPolygon.enable();
+}
+
+function getGeoJson(){
+    var drawnFeaturesJSON = drawnItems.toGeoJSON();
+    var i = 0;
+    drawnItems.eachLayer(function(layer){
+        layer.bindPopup(i+"");
+        drawnFeaturesJSON.features[i].properties = layer.properties;
+        i++;
+    });
+    document.getElementById("JSONBox").value = JSON.stringify(drawnFeaturesJSON);
+    myDataRef.set(drawnFeaturesJSON);
+}
+
+function saveGeoJson(){
+    var drawnFeaturesJSON = drawnItems.toGeoJSON();
+    var i = 0;
+    drawnItems.eachLayer(function(layer){
+        layer.bindPopup(i+"");
+        drawnFeaturesJSON.features[i].properties = layer.properties;
+        i++;
+    });
+    document.getElementById("JSONBox").value = JSON.stringify(drawnFeaturesJSON);
+    userRef.update(drawnFeaturesJSON);
+}
